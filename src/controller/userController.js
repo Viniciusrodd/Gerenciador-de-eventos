bad19d50446c8aa51a29ddbce3e26fc1c055b92b
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-11 17:12:49 -0300

    Instalando + configurando Multer

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,23 @@
 
 const express = require('express');
 const router = express.Router();
+const recordsModel = require('../models/recordsModel');
+const multer = require('multer');
+
+
+//config do multer
+const storage = multer.memoryStorage(); // Armazena a imagem na memória
+const image = multer({ 
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (!allowedTypes.includes(file.mimetype)) {
+            return cb(new Error('Tipo de arquivo não permitido. Envie apenas imagens jpeg, png ou gif.'));
+        }
+        cb(null, true);
+    }
+ });
+
 
 
 router.get('/register', (req, res) =>{
@@ -8,7 +25,16 @@ router.get('/register', (req, res) =>{
 })
 
 
-router.post('/saveRecords', (req, res) =>{
+
+router.post('/saveRecords', image.single('imageFile') , async (req, res) =>{
+    try{
+
+    }
+    catch(error){
+        console.error(`Erro ao criar registro: ${error}`);
+        res.status(500).send('Erro ao processar o registro');
+    }
+    
     var {nome, email, senha} = req.body;
 
     res.json({nome, email, senha})
