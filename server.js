b3cee6fee0e24fa597e1ddefdba716c730b59cfe
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-11 11:54:03 -0300

    Definindo e configurando primeira rota

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = require('./src/routes/routes');
+const path = require('path');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.use('/', router);
+
+
+app.listen(7070, () =>{
+    console.log('Event manager running')
+});
