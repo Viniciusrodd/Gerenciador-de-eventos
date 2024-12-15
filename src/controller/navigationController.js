6819e5f43bbe31c6b06de1c790f539c68ff79678
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-15 13:48:53 -0300

    Incluindo dados de relação de tabelas para serem renderizados juntos

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const recordModel = require('../models/recordModel');
+const eventModel = require('../models/eventModel');
 const sequelize = require('sequelize');
 const userAuth = require('../middleware/userAuth');
 
@@ -16,15 +17,29 @@ router.get('/login', (req, res) =>{
 
 
 router.get('/homepage', userAuth, (req, res) =>{
-    if(req.session.user){
-        const user = req.session.user;
-
-        res.render('homepage', {
-            userData: user
-        });
-    }else{
-        res.redirect('/login')
-    }
+    eventModel.findAll({
+        ordere: [
+            ['id', 'DESC']
+        ],
+        include: [
+            {
+                model: recordModel,
+                attributes: ['fullName', 'userName', 'email']
+            }
+        ]
+    })
+    .then((data) =>{
+        if(req.session.user){
+            const user = req.session.user;
+    
+            res.render('homepage', {
+                dadosEvents: data,
+                userData: user
+            });
+        }else{
+            res.redirect('/login')
+        }
+    })
 })
 
 router.get('/criarEventos', userAuth,(req, res) =>{
