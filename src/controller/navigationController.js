0bccba46e75501e299206db1cce4b5ab04b8495a
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2024-12-27 18:08:14 -0300

    Reformulando lógica na rota de exibição de dados na homepage para correção de bugs

--- src/controller/navigationController.js
+++ src/controller/navigationController.js
@@ -18,68 +18,62 @@ router.get('/login', (req, res) =>{
 })
 
 
-router.get('/homepage', userAuth, (req, res) =>{
-    eventModel.findAll({
-        order: [
-            ['id', 'DESC']
-        ],
-        include: [
-            {
-                model: recordModel,
-                as: 'records', // Alias usado na associação
-                attributes: ['fullName', 'userName', 'image']
-                //where: {id: req.session.user.id}
+router.get('/homepage', userAuth, async (req, res) =>{
+    try{
+        var eventData = await eventModel.findAll({
+            order: [
+                ['id', 'DESC']
+            ]
+        })
+        var profileData = await recordModel.findAll()
+        var userIdSession = req.session.user.id;
+
+        profileData.forEach(data =>{
+            if (data && data.image) {
+                // Convertendo a imagem da tabela records para base64
+                data.profileimage = Buffer.from(data.image).toString('base64');
+                data.profileimage = `data:image/jpeg;base64,${data.profileimage}`
             }
-        ]
-    })
-    .then((data) =>{
-        if(req.session.user){
-            const user = req.session.user;
-            //se houver uma imagem -> para base64
-            data.forEach(event => {
-                if (event.records && event.records.image) {
-                    // Convertendo a imagem da tabela records para base64
-                    event.records.profileimage = Buffer.from(event.records.image).toString('base64');
-                    event.records.profileimage = `data:image/jpeg;base64,${event.records.profileimage}`
-                }
+        })
 
-                if (event.image ) {
-                    event.imageBase64 = Buffer.from(event.image).toString('base64');
-                    event.imageEvent = `data:image/jpeg;base64,${event.imageBase64}`
-                }
+        eventData.forEach(event => {
+            if (event.image ) {
+                event.imageBase64 = Buffer.from(event.image).toString('base64');
+                event.imageEvent = `data:image/jpeg;base64,${event.imageBase64}`
+            }
 
-                // Trabalhando com a data
-                const eventDate = moment(event.data).tz('America/Sao_Paulo', true);
-                event.day = eventDate.date();  // Extrai o dia
-                event.month = eventDate.month() + 1;  // Extrai o mês (0-11, por isso adicionamos 1)
-                event.year = eventDate.year();  // Extrai o ano
-                
+            // Trabalhando com a data
+            const eventDate = moment(event.data).tz('America/Sao_Paulo', true);
+            event.day = eventDate.date();  // Extrai o dia
+            event.month = eventDate.month() + 1;  // Extrai o mês (0-11, por isso adicionamos 1)
+            event.year = eventDate.year();  // Extrai o ano
+        
+            // Trabalhando com o horário
+            if (event.hora_inicio) { // Valida se o campo de horário de início existe
+                const startTime = moment(event.hora_inicio, 'HH:mm:ss').tz('America/Sao_Paulo', true);
+                event.startHours = startTime.hours();
+                event.startMinutes = startTime.minutes();
+                event.startSeconds = startTime.seconds();
+            }
 
+            if (event.hora_fim) { // Valida se o campo de horário de fim existe
+                const endTime = moment(event.hora_fim, 'HH:mm:ss').tz('America/Sao_Paulo', true);
+                event.endHours = endTime.hours();
+                event.endMinutes = endTime.minutes();
+                event.endSeconds = endTime.seconds();
+            }
+        });
 
-                // Trabalhando com o horário
-                if (event.hora_inicio) { // Valida se o campo de horário de início existe
-                    const startTime = moment(event.hora_inicio, 'HH:mm:ss').tz('America/Sao_Paulo', true);
-                    event.startHours = startTime.hours();
-                    event.startMinutes = startTime.minutes();
-                    event.startSeconds = startTime.seconds();
-                }
-
-                if (event.hora_fim) { // Valida se o campo de horário de fim existe
-                    const endTime = moment(event.hora_fim, 'HH:mm:ss').tz('America/Sao_Paulo', true);
-                    event.endHours = endTime.hours();
-                    event.endMinutes = endTime.minutes();
-                    event.endSeconds = endTime.seconds();
-                }
-            });
-
-            res.render('homepage', {
-                dadosEvents: data,
-                userData: user
-            });
-        }else{
-            res.redirect('/login')
-        }
-    })
+        res.render('homepage', {
+            dadosEvents: eventData,
+            dadosProfile: profileData,
+            userId: userIdSession
+        });    
+    }
+    catch(error){
+        console.log('Error at homepage', error)
+        return res.redirect('/login')
+    }         
 })
 
 
